cd4255404adcc631e2dfbad36dca0c60912e05d8
Martin Loetzsch <martin.loetzsch@gmail.com>
2022-07-31 21:44:29 +0200

    Add MUI material design components and define basic UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+const card = (
+  <React.Fragment>
+  </React.Fragment>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Rick and Morty Explorer</h1>
+        <p>Find your favourite Rick and Morty character!</p>
+        <Card variant="outlined">
+          <CardContent>
+            <img className="Card-img" src="https://i.guim.co.uk/img/media/b563ac5db4b4a4e1197c586bbca3edebca9173cd/0_12_3307_1985/master/3307.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=61a26bf43da26e4ca97e932e5ee113f7"/>
+          </CardContent>
+        </Card>
+        <br/>
+        <Button variant="contained">Show me another one!</Button>
       </header>
     </div>
   );
